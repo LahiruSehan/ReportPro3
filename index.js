8904840eb52fef5759a671f7e71c553d4c754fd1
LahiruSehan <158451116+LahiruSehan@users.noreply.github.com>
2026-02-03 08:05:27 +0000

    your commit message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 class ZohoLedgerApp {
   constructor() {
-    this.proxyPrefix = "https://corsproxy.io/?";
+    this.proxyPrefix = "https://api.allorigins.win/raw?url=";
     this.initStorage();
     this.state = {
       accessToken: localStorage.getItem('zoho_access_token'),
